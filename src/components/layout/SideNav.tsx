086ed46427073e6c1f317114a38972a01bde815a
MAC <jlucky046@gmail.com>
2024-08-18 11:22:03 +0000

    update to dashboard

--- src/components/layout/SideNav.tsx
+++ src/components/layout/SideNav.tsx
@@ -2,13 +2,14 @@
 
 import { ChevronDown, ChevronRight, Clock, Home, Settings, User } from "lucide-react";
 import Link from "next/link";
-import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+import React, { useState, useEffect } from "react";
 
 const navItems = [
     {
         title: "Home",
         icon: <Home />,
-        link: "/dash",
+        link: "/dashboard",
         children: [],
     },
     {
@@ -16,30 +17,39 @@ const navItems = [
         icon: <Clock />,
         link: "/history",
         children: [
-            { title: "P2P Order", link: "/history/p2p" },
-            { title: "Transaction History", link: "/history/transaction" },
-            { title: "Appealed Order", link: "/history/appeals" },
+            { title: "P2P Orders", link: "/dashboard/history/orders" },
+            { title: "Transactions", link: "/dashboard/history/transactions" },
+            { title: "Appealed Order", link: "/dashboard/history/appeals" },
         ],
     },
     {
         title: "Account",
         icon: <User />,
-        link: "/settings",
+        link: "/account",
         children: [
-            { title: "Identification", link: "/settings/profile" },
-            { title: "Payment", link: "/settings/account" },
+            { title: "Identification", link: "/dashboard/account/identification" },
+            { title: "Payment", link: "/dashboard/account/payment" },
         ],
     },
     {
         title: "Settings",
         icon: <Settings />,
-        link: "/settings",
+        link: "/dashboard/settings",
         children: [],
     },
 ];
 
 const SideNav = () => {
+    const pathname = usePathname();
     const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+    useEffect(() => {
+        navItems.forEach((item, index) => {
+            if (item.children.some((child) => pathname.startsWith(child.link))) {
+                setActiveIndex(index);
+            }
+        });
+    }, [pathname]);
 
     const handleNavClick = (index: number) => {
         setActiveIndex(activeIndex === index ? null : index);
@@ -50,26 +60,30 @@ const SideNav = () => {
             <ul className="space-y-6">
                 {navItems.map((item, index) => (
                     <li key={index}>
-                        <div
-                            className={`flex items-center p-4 cursor-pointer ${activeIndex === index ? "bg-gray-50" : ""
-                                }`}
-                            onClick={() => handleNavClick(index)}
-                        >
-                            <div className="mr-3">{item.icon}</div>
-                            <div className="flex-1">{item.title}</div>
-                            {item.children.length > 0 && (
+                        {item.children.length > 0 ? (
+                            <div
+                                className={`flex items-center p-4 cursor-pointer ${activeIndex === index ? "bg-gray-100 " : ""}`}
+                                onClick={() => handleNavClick(index)}
+                            >
+                                <div className="mr-3">{item.icon}</div>
+                                <div className="flex-1">{item.title}</div>
                                 <div>
                                     {activeIndex === index ? <ChevronDown /> : <ChevronRight />}
                                 </div>
-                            )}
-                        </div>
+                            </div>
+                        ) : (
+                            <Link href={item.link} className={`flex items-center p-4 hover:bg-gray-50 ${pathname === item.link ? "font-bold" : ""}`} onClick={() => handleNavClick(index)}>
+                                <div className="mr-3">{item.icon}</div>
+                                <div className="flex-1">{item.title}</div>
+                            </Link>
+                        )}
                         {activeIndex === index && item.children.length > 0 && (
                             <ul className="ml-8 mt-2 space-y-6">
                                 {item.children.map((child, childIndex) => (
                                     <li key={childIndex}>
                                         <Link
                                             href={child.link}
-                                            className="block p-2 hover:bg-gray-50 text-gray-600"
+                                            className={`block p-2 hover:bg-gray-50 text-gray-600 ${pathname === child.link ? "font-bold" : ""}`}
                                         >
                                             {child.title}
                                         </Link>
@@ -84,4 +98,4 @@ const SideNav = () => {
     );
 }
 
-export default SideNav
+export default SideNav;
