c201e08c61c08bade5c177c7cdc51a1b3189f2ee
MAC <jlucky046@gmail.com>
2024-08-18 13:17:09 +0000

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,65 @@
+import { steps } from '@/common/data/steps'
+import Verify from '@/components/cards/Verify'
+import Steps from '@/components/step'
+import Button from '@/components/ui/Button'
+import { Clock, EyeOff, SquareAsterisk } from 'lucide-react'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='flex flex-col justify-start gap-10'>
+      <h1 className='text-2xl font-medium'>Get Started</h1>
+      {/* <Steps steps={steps} /> */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="">
+          <Verify />
+        </div>
+        <div className="flex flex-col justify-between border border-gray-500 rounded-xl p-6">
+          <h1 className="text-xl font-medium">
+            Trade Crypto above $500
+          </h1>
+          <div className='flex flex-row items-center gap-3'>
+            <Clock className='text-gray-500 w-6 h-6' />
+            <span className='text-gray-500'>Pending</span>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-between border border-gray-500 rounded-xl p-6">
+          <h1 className="text-xl font-medium">
+            Stake to Become Merchant and Settler
+          </h1>
+          <div className='flex flex-row items-center gap-3'>
+            <Clock className='text-gray-500 w-6 h-6' />
+            <span className='text-gray-500'>Pending</span>
+          </div>
+        </div>
+
+      </div>
+      <div className="flex flex-col justify-between border border-gray-200 rounded-xl p-6 gap-4">
+        <div className="flex flex-row items-center gap-4">
+          <span className="text-xl font-medium">
+            Estimated Balance
+          </span>
+          <EyeOff />
+        </div>
+        <div className='flex flex-col items-start gap-4'>
+          <div className="flex flex-row items-center gap2">
+            <SquareAsterisk className='text-gray-400' />
+            <SquareAsterisk className='text-gray-400' />
+            <SquareAsterisk className='text-gray-400' />
+            <SquareAsterisk className='text-gray-400' />
+            <SquareAsterisk className='text-gray-400' />
+            <SquareAsterisk className='text-gray-400' />
+          </div>
+          <Button
+            text="Add Wallet"
+            icon="/images/icons/add-circle.png"
+            iconPosition="right"
+            className='bg-black text-white px-3 py-2 rounded-xl'
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/cards/Verify.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from '../ui/Button'
+import Image from 'next/image'
+
+const Verify = () => {
+    return (
+        <div className='flex flex-col flex-col-reverse lg:flex-row border border-blue-700 rounded-xl p-6 bg-blue-50 w-full'>
+            <div className='flex flex-col justify-start items-start gap-6'>
+                <div className='flex flex-col gap-2'>
+                    <h1 className='text-xl font-medium'>Verify Your Account</h1>
+                    <p className='text-gray-500 max-w-[300px]'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremquelauda</p>
+                </div>
+
+                <Button
+                    text="Proceed to KYC"
+                    icon="/images/icons/export.svg"
+                    iconPosition="right"
+                    className='bg-black text-white px-3 py-2 rounded-xl'
+                />
+            </div>
+            <Image src="/images/icons/verify.png" alt="verify" width={139} height={139} className='flex-shrink-0' />
+        </div>
+    )
+}
+
+export default Verify
