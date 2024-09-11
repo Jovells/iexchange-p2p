ce16fd07f125c6fab98f0a86c341e0b83d8f628c
MAC <jlucky046@gmail.com>
2024-09-11 06:46:08 +0000

    ui updates

--- src/app/dashboard/account/payment/page.tsx
+++ src/app/dashboard/account/payment/page.tsx
@@ -6,6 +6,7 @@ import Button from '@/components/ui/Button';
 import SearchInput from '@/components/ui/SearchInput';
 import { Check, X } from 'lucide-react';
 import React, { useEffect, useState } from 'react';
+import AddPaymentMethod from './AddPaymentMethod';
 
 type PaymentMethod = {
   id: number;
@@ -41,55 +42,56 @@ const Payment = () => {
   // Function to show the modal with the correct content
   const handleClick = () => {
     showModal(
-      <div className='w-full lg:w-[800px] h-auto bg-white rounded-xl shadow-md border-2 border-gray-500 flex flex-col gap-4'>
-        <div className='flex flex-row items-center justify-between border-b px-8 py-6'>
-          <h1 className='font-bold'>Select Payment Method</h1>
-          <div className='flex justify-end'>
-            <X onClick={hideModal} className='cursor-pointer' />
-          </div>
-        </div>
-        <div className='gap-4 flex flex-col p-8 pt-4'>
-          {/* <Loader size="large" color="#3498db" className="my-4" /> */}
-          <SearchInput
-            placeholder="Type to search..."
-            onSearch={handleSearch}
-            showButton={false}
-            buttonText="Go"
-          />
-          <ul className="space-y-2">
-            {paymentMethods.map((method) => (
-              <li
-                key={method.id}
-                className="flex justify-between items-center p-3 border border-gray-300 rounded-lg cursor-pointer hover:bg-gray-100"
-                onClick={() => toggleSelection(method.id)}
-              >
-                <span>{method.name}</span>
-                {selectedMethods.includes(method.id) && (
-                  <div className='bg-black rounded-lg p-1 flex items-center justify-center'>
-                    <Check className="h-3 w-3 text-white" />
-                  </div>
-                )}
-              </li>
-            ))}
-          </ul>
-          <div className='flex flex-row items-center gap-4 p-4 px-0'>
-            <Button
-              text="Cancel"
-              className="flex-1 bg-transparent text-black border border-gray-500 hover:bg-gray-100rounded-xl px-4 py-2"
-              onClick={() => {
-                hideModal()
-                setSelectedMethods([])
-              }}
-            />
-            <Button
-              text="Add Payment Method"
-              className="flex-1 bg-black text-white hover:bg-gray-600 rounded-xl px-4 py-2"
-              onClick={handleClick}
-            />
-          </div>
+      <AddPaymentMethod hideModal={hideModal} />
+      // <div className='w-full lg:w-[800px] h-auto bg-white rounded-xl shadow-md border-2 border-gray-500 flex flex-col gap-4'>
+      //   <div className='flex flex-row items-center justify-between border-b px-8 py-6'>
+      //     <h1 className='font-bold'>Select Payment Method</h1>
+      //     <div className='flex justify-end'>
+      //       <X onClick={hideModal} className='cursor-pointer' />
+      //     </div>
+      //   </div>
+      //   <Loader className='h-[400px]' />
+      //   {/* <div className='gap-4 flex flex-col p-8 pt-4'>
+      //     <SearchInput
+      //       placeholder="Type to search..."
+      //       onSearch={handleSearch}
+      //       showButton={false}
+      //       buttonText="Go"
+      //     />
+      //     <ul className="space-y-2">
+      //       {paymentMethods.map((method) => (
+      //         <li
+      //           key={method.id}
+      //           className="flex justify-between items-center p-3 border border-gray-300 rounded-lg cursor-pointer hover:bg-gray-100"
+      //           onClick={() => toggleSelection(method.id)}
+      //         >
+      //           <span>{method.name}</span>
+      //           {selectedMethods.includes(method.id) && (
+      //             <div className='bg-black rounded-lg p-1 flex items-center justify-center'>
+      //               <Check className="h-3 w-3 text-white" />
+      //             </div>
+      //           )}
+      //         </li>
+      //       ))}
+      //     </ul>
+      //     <div className='flex flex-row items-center gap-4 p-4 px-0'>
+      //       <Button
+      //         text="Cancel"
+      //         className="flex-1 bg-transparent text-black border border-gray-500 hover:bg-gray-100rounded-xl px-4 py-2"
+      //         onClick={() => {
+      //           hideModal()
+      //           setSelectedMethods([])
+      //         }}
+      //       />
+      //       <Button
+      //         text="Add Payment Method"
+      //         className="flex-1 bg-black text-white hover:bg-gray-600 rounded-xl px-4 py-2"
+      //         onClick={handleClick}
+      //       />
+      //     </div>
 
-        </div>
-      </div>
+      //   </div> */}
+      // </div>
     );
   };
 
--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,26 +1,16 @@
-'use client';
+'use client'
 
-import React, { FC } from 'react';
+import React from 'react';
 
-type LoaderProps = {
-    size?: 'small' | 'medium' | 'large'; // Different sizes for the loader
-    color?: string; // Customize the color of the loader
-    className?: string; // Optional prop to allow for additional custom styling
-};
+interface Props {
+    className?: string;
+}
 
-const Loader: FC<LoaderProps> = ({ size = 'medium', color = '#000', className = '' }) => {
-    // Set size classes based on the provided size prop
-    const sizeClasses = {
-        small: 'h-4 w-4',
-        medium: 'h-8 w-8',
-        large: 'h-12 w-12',
-    };
-
+const Loader: React.FC<Props> = ({ className }) => {
     return (
-        <div
-            className={`inline-block border-4 border-t-transparent rounded-full animate-spin ${sizeClasses[size]} ${className}`}
-            style={{ borderColor: color }} // Use the provided color for the loader
-        />
+        <div className={`flex items-center justify-center ${className}`}>
+            <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+        </div>
     );
 };
 
