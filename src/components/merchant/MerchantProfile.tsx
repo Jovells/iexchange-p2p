edf25a963a358fea29ffbc6834becf1b0167e8fb
Jovells <jovells.appiah@gmail.com>
2024-09-24 13:20:29 +0000

    added payment method

--- src/components/merchant/MerchantProfile.tsx
+++ src/components/merchant/MerchantProfile.tsx
@@ -10,14 +10,22 @@ const MerchantProfile = ({
         <div className='flex flex-col justify-between h-full px-6 pl-0'>
             <div className='space-y-4'>
                 <div className="flex flex-row items-center">
+                    {/* TODO@mbawon */}
                     <IndianRupee className='w-4 h-4' />
                     <span>{offer.merchant.name} ({shortenAddress(offer.merchant.id)})</span>
                     <BadgeCheck className='w-4 h-4 ml-1' />
                 </div>
                 {/* trades */}
-                <div className='text-xs'>
+                <div className='text-xs flex gap-20'>
+                    <div>
                     <p>397 trades</p>
                     <p>99% Completion</p>
+                    </div>
+                    <div>
+                    {/* TODO: @mbawon Check alignment */}
+                    <p className='font-bold text-gray-200 bg-slate-500 rounded p-1  '>{offer.paymentMethod.method}</p>
+                    <p>Payment Method</p>
+                    </div>
                 </div>
                 {/* commitments */}
                 <div className='flex flex-col justify-start items-start lg:flex-row gap-2 lg:gap-20 text-xs'>
@@ -30,7 +38,7 @@ const MerchantProfile = ({
                         <span>Avg. Pay Time</span>
                     </div>
                     <div className='flex flex-row-reverse justify-between lg:flex-col w-full lg:w-auto'>
-                        <span>{formatCurrency(offer.maxOrder, offer.currency.currency)}</span>
+                        <span>{formatCurrency(offer.minOrder, offer.token.symbol)} - {formatCurrency(offer.maxOrder, offer.token.symbol)}</span>
                         <span>Limit</span>
                     </div>
                 </div>
