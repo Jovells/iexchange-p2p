93034dabd5f9367ed823f4ea1ec56fcb9de62dc1
MAC <jlucky046@gmail.com>
2024-10-10 08:35:15 +0000

    fixed

--- src/components/shared/modal/Modal.tsx
+++ src/components/shared/modal/Modal.tsx
@@ -7,10 +7,19 @@ import { useEffect, useState } from "react";
 const ModalManager: React.FC = () => {
   const { modalContent, modalProps, hideModal } = useModal();
   const [isMounted, setIsMounted] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
+
+  useEffect(() => {
+    if (modalContent) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  }, [modalContent]);
 
   if (!isMounted || modalContent === null) {
     return null;
@@ -25,7 +34,8 @@ const ModalManager: React.FC = () => {
       ></div>
       <div
         className={classnames(
-          "relative rounded-lg z-20 p-6",
+          "relative rounded-lg z-20 p-6 transition-transform transition-opacity duration-300 ease-in-out transform",
+          isVisible ? "opacity-100 scale-100" : "opacity-0 scale-95",
           modalProps?.hasPadding
             ? "bg-white border border-gray-300 text-black dark:bg-gray-800 dark:border-gray-600 dark:text-white"
             : ""
