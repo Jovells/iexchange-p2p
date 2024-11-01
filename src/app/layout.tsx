079f64f4f4be9a498e2a7d468e6d675c80b1d579
MAC <jlucky046@gmail.com>
2024-11-01 12:23:16 +0000

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/common/contexts/ThemeProvider";
 import { Suspense } from "react";
 import PostAd from "./(trade)/postAd";
+import ScrollToTopOnPageChange from "@/components/layout/ScrollToTop";
 
 const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto({
@@ -41,12 +42,11 @@ export default function RootLayout({
               <ModalContextProvider>
                 <Toaster />
                 <Suspense fallback={<TopLoader />}>
-                <div>
                   {children}
                   <PostAd className="block lg:hidden" text="Post Ad" />
-                </div>
                 </Suspense>
                 <ModalManager />
+                <ScrollToTopOnPageChange />
               </ModalContextProvider>
             </WalletProvider>
           </ThemeProvider>
