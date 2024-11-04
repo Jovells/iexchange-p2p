c4b6d38e3f68612f19872d11d3cf327e2de00fce
Jovells <jovells.appiah@gmail.com>
2024-11-04 00:28:32 +0000

    updated

--- src/app/(trade)/layout.tsx
+++ src/app/(trade)/layout.tsx
@@ -2,43 +2,45 @@
 
 import "@/styles/globals.css";
 import "@/styles/custom.css";
-import { useState, useEffect, lazy, Suspense } from "react";
+import { lazy } from "react";
 import { usePathname } from "next/navigation";
 
 const MainNav = lazy(() => import("@/components/layout/navbar"));
 const SubNav = lazy(() => import("@/components/layout/navbar/SubNav"));
 const Footer = lazy(() => import("@/components/layout/footer"));
-const Loader = lazy(() => import("@/components/loader/Loader"));
 
 const TradeLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-    const [isLoading, setIsLoading] = useState(true);
-    const path = usePathname()
+  const path = usePathname();
 
-    // useEffect(() => {
-    //     const timer = setTimeout(() => {
-    //         setIsLoading(false);
-    //     }, 1000);
+  // useEffect(() => {
+  //     const timer = setTimeout(() => {
+  //         setIsLoading(false);
+  //     }, 1000);
 
-    //     return () => clearTimeout(timer);
-    // }, []);
+  //     return () => clearTimeout(timer);
+  // }, []);
 
-    return (
-      <div className="relative min-h-screen flex flex-col">
-        {/* {isLoading ? (
+  return (
+    <div className="relative min-h-screen flex flex-col">
+      {/* {isLoading ? (
           <Loader loaderType="spinner" className="h-screen" />
         ) : ( */}
-          <>
-            <div className="fixed top-0 left-0 right-0 z-50 border-b border-gray-300 dark:border-gray-800 bg-white dark:bg-[#14161B]">
-              <MainNav />
-              <SubNav />
-            </div>
+      <>
+        <div className="fixed top-0 left-0 right-0 z-50 border-b border-gray-300 dark:border-gray-800 bg-white dark:bg-[#14161B]">
+          <MainNav />
+          <SubNav />
+        </div>
 
-            {<div className={`lex-grow pt-[100px] ${path.includes("dashboard") && "pr-0 lg:pr-6 pl-0 lg:pl-[260px]"}`}>{children}</div>}
-            {!path.includes("dashboard") && <Footer />}
-          </>
-        {/* )} */}
-      </div>
-    );
-}
+        {
+          <div className={`lex-grow pt-[100px] ${path.includes("dashboard") && "pr-0 lg:pr-6 pl-0 lg:pl-[260px]"}`}>
+            {children}
+          </div>
+        }
+        {!path.includes("dashboard") && <Footer />}
+      </>
+      {/* )} */}
+    </div>
+  );
+};
 
 export default TradeLayout;
